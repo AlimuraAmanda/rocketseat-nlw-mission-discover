c4212f372d4966d2aa2fe31b9750bf65eb2e21b8
AlimuraAmanda <amanda.alimura@gmail.com>
2021-08-24 17:07:01 -0300

    adição de rotas e validação de senha

--- src/route.js
+++ src/route.js
@@ -1,20 +1,22 @@
 const express = require('express')
 
-const QuestionController = require('./controllers/question-controller')
+const QuestionController = require('./controllers/QuestionController')
 
-const RoomCrontroller = require('./controllers/RoomCrontroller')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router()
 
 route.get('/', (req, res) => res.render('index', { page: 'enter-room' }))
 
-route.get('/room/:room', (req, res) => res.render('room'))
-
 route.get('/create-pass', (req, res) =>
   res.render('index', { page: 'create-pass' })
 )
 
+route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
+route.post('/enteroom', RoomController.enter)
+
+route.post('/question/create/:room', QuestionController.create)
 route.post('/question/:room/:question/:action', QuestionController.index)
-route.post('/create-room', RoomCrontroller.create)
 
 module.exports = route
